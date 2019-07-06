f18fdc37efc68d2e8f42abea81922728ee177281
eyalz800 <eyalz800@gmail.com>
2019-07-06 18:37:46 +0300

    Added includes and header guards in needed places.
    Change-Id: Iea9785ea950072de2b8cce0b02c3c0499956c879

--- proc.h
+++ proc.h
@@ -1,3 +1,9 @@
+#include "types.h"
+#include "mmu.h"
+
+#ifndef XV6_PROC_H
+#define XV6_PROC_H
+
 // Per-CPU state
 struct cpu {
   uchar apicid;                // Local APIC ID
@@ -72,3 +78,5 @@ struct proc {
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
+
+#endif
