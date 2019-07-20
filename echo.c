ebf9625cc70f20da61fe64b79ce5b25f166bd463
eyalz800 <eyalz800@gmail.com>
2019-07-21 00:26:39 +0300

    Made echo write input in one write call, as well as support for no newlines using -n switch.
    Change-Id: Ib968de9adb722ebc180e4008d6d6c6f2c451d93a

--- echo.c
+++ echo.c
@@ -5,9 +5,47 @@
 int
 main(int argc, char *argv[])
 {
-  int i;
+  int i = 1;
+  int size = 1000;
+  char * data = malloc(size);
+  int offset = 0;
+  char new_line = 1;
 
-  for(i = 1; i < argc; i++)
-    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+  if (!data) {
+    exit(1);
+  }
+
+  if (argc > 1 && !strcmp("-n", argv[1])) {
+    ++i;
+    new_line = 0;
+  }
+
+  for(; i < argc; i++) {
+    int length = strlen(argv[i]);
+    if (length + offset > size) {
+      char * newdata = 0;
+      size = length + size * 3 / 2;
+      newdata = malloc(size);
+      if (!newdata) {
+        free(data);
+        exit(1);
+      }
+      memmove(newdata, data, offset);
+      free(data);
+      data = newdata;
+    }
+    memmove(data + offset, argv[i], length);
+    offset += length;
+    if (i + 1 < argc) {
+      data[offset] = ' ';
+      ++offset;
+    } else if (new_line) {
+      data[offset] = '\n';
+      ++offset;
+    }
+  }
+
+  write(1, data, offset);
+  free(data);
   exit(0);
 }
