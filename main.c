e7423018bdd660dac59e13d9cb51fe6239575c5a
MichaelSimkin <michael.simkin22@gmail.com>
2019-08-13 22:22:58 +0300

    Added cgroups, cgroup table protected by spinlock, cgroup manipulation functions, cgroup related fields to proc struct.
    Change-Id: I959217ad968dda6aa18f09be6bcae8544d34a518

--- main.c
+++ main.c
@@ -38,6 +38,7 @@ main(void)
   binit();         // buffer cache
   fileinit();      // file table
   ideinit();       // disk 
+  cginit();        // group table
   startothers();   // start other processors
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   userinit();      // first user process
