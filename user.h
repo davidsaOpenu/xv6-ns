ad6ed4a857045656efaf3b5f77b25e8de276ba64
houdini91 <mdstrauss91@gmail.com>
2019-08-31 13:42:08 +0300

    Added containers support
    - pouch - creates and runs containers
    - Added ioctl syscall to enable disable ttys
    - Added tty device support
    - Added cgroups support
    
    Change-Id: Ie0025d3b55ee53232710615842db63dda9d23793

--- user.h
+++ user.h
@@ -52,5 +52,14 @@ void* malloc(uint);
 void free(void*);
 int atoi(const char*);
 int itoa(char *, int);
+char* strcat(char* dest, const char* source);
+
+int attach_tty(int tty_fd);
+int detach_tty(int tty_fd);
+int connect_tty(int tty_fd);
+int is_attached_tty(int tty_fd);
+int disconnect_tty(int tty_fd);
+int is_connected_tty(int tty_fd);
+
 
 #endif
