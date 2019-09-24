62d119146a4a51e79581e4eb311a902bdaa87cc5
MichaelSimkin <michael.simkin22@gmail.com>
2019-09-24 22:02:16 +0300

    Added cgroup file manipulation functionality
    - Functions for manipulating cgroup files
    - New fields in file struct for cgroup support
    - New function in proc.c for moving processes between cgroups.
    
    Change-Id: I6913e8849ace200ef5cf0f7251d67f5d2cd17fd5

---
+++ cgfs.c
@@ -0,0 +1,14 @@
+#include "cgfs.h"
+#include "defs.h"
+#include "fcntl.h"
+#include "file.h"
+#include "fs.h"
+#include "param.h"
+#include "spinlock.h"
+#include "types.h"
+
+#define MAX_PID_LENGTH 5
+#define MAX_CGROUP_DIR_ENTRIES 64
+
+
+
