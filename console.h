b8d7699ae41b428b10b160cc91a83b4615eb7fb0
Home <arkady_bolotin@hotmail.com>
2020-09-15 15:48:58 +0300

    Implement new console functionality.
    Add support for speical keyboard keys such as arrow up and down.
    Support of these keys is added to both regular qemu version and qemu-nox.
    
    In addition, implements command history support and predefined history
    for console.
    
    Also, add support for console commands 'help', 'quit' and 'reboot'.
    
    Change-Id: I4fc921a406235a0d9806a9f3e5acb78bcf2d71e9

---
+++ console.h
@@ -0,0 +1,32 @@
+#define INPUT_BUF 128 //Maximum length of possible input.
+#define HIST_BUF 5 //Size of history buffer (how many commands it can remember).
+#define C(x)  ((x)-'@')  // Control-x
+
+//The special key codes for qemu will be defined here.     
+#define HOME            0xE0
+#define END             0xE1
+#define ARROW_UP        0xE2
+#define ARROW_DOWN      0xE3
+#define ARROW_LEFT      0xE4
+#define ARROW_RIGHT     0xE5
+#define PAGEUP          0xE6
+#define PAGEDOWN        0xE7
+#define INSERT          0xE8
+#define DELETE          0xE9
+
+//The special key codes for qemu-nox will be defined here.
+#define ARROW_UP_NOX    'A'
+#define ARROW_DOWN_NOX  'B'
+#define ARROW_RIGHT_NOX 'C'
+#define ARROW_LEFT_NOX  'D'
+#define END_NOX         'F'
+#define HOME_NOX        'H'
+#define INSERT_NOX      '2'
+#define DELETE_NOX      '3'
+#define PAGEUP_NOX      '5'
+#define PAGEDOWN_NOX    '6'
+
+//Predefined history here - if adding new commands, update PREDEF_COUNT as well to reflect the correct number.
+//Remember maximum number of commands defined in HIST_BUF above.
+#define PREDEF_COUNT 1
+const char *predef_hist[] = {"cgroupstests"};
