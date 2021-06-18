38126e4df99ba5550fb99764ffaf5904e030954e
FinalHopee <finalhopee@gmail.com>
2021-06-18 10:27:54 -0700

    cgroup memory controller - memory.min
    Add support for memory.min
    Add new proc function for memory grow
    Add function to cgroup that checks ancestry
    
    Tests:
    test_limiting_mem
    test_cant_shrink_over_mem_min_limit
    test_proc_mem_grows_to_match_mem_min_limit
    test_mem_min_eq_mem_max
    test_procs_grow_to_match_mem_min
    
    Change-Id: Ida2a21851516537611830a0b6f8e4303aaef281f

--- file.h
+++ file.h
@@ -72,6 +72,10 @@ struct file {
             char active;
             unsigned int max;
           } max;
+          struct {
+            char active;
+            unsigned int min;
+          } min;
         } mem;
       };
     };
