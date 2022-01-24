b2def6605e0b373f38db0d103f67c914b4807537
Baruch-Fridman <bruchfridman@gmail.com>
2022-01-24 12:20:05 -0800

    cgroup memory controller - memory.min
    Add support for memory.min
    When declaring memory.min updates a counter of saved pages that can only be assigned to this group.
    If there are not enough free pages to save the statement you will fail.
    If you try to allocate memory, such as creating a process, but there are no unsaved pages [the number of free pages equals the number of saved pages] the request will be rejected.
    When allocating memory to a group for which pages have been saved, the number of saved pages will decrease.
    
    Change-Id: I13b1b5c417c97a89067fb647608569694f5269e2

--- cgroupstests.h
+++ cgroupstests.h
@@ -6,11 +6,15 @@
 
 enum controller_types { CPU_CNT, PID_CNT, SET_CNT, MEM_CNT };
 
+#define KERNBASE "2147483648" //for memory controler test
+#define PAGE_SIZE 4096
+#define MEM_SIZE 56320*PAGE_SIZE
 
 #define CGROUP                          "cgroup"
 #define ROOT_CGROUP                     "/cgroup"
 #define TEST_1                          "/cgroup/test1"
 #define TEST_2                          "/cgroup/test2"
+#define TEST_TMP                        "/cgroup/testtmp"
 #define TEST_1_1                        "/cgroup/test1.1"
 #define TEST_1_2                        "/cgroup/test1.2"
 
@@ -30,9 +34,15 @@ enum controller_types { CPU_CNT, PID_CNT, SET_CNT, MEM_CNT };
 #define TEST_1_SET_FRZ                  "/cgroup/test1/cgroup.freeze"
 #define TEST_1_MEM_CURRENT              "/cgroup/test1/memory.current"
 #define TEST_1_MEM_MAX                  "/cgroup/test1/memory.max"
+#define TEST_1_MEM_MIN                  "/cgroup/test1/memory.min"
 #define TEST_1_MEM_STAT                 "/cgroup/test1/memory.stat"
 
+#define TEST_2_CGROUP_SUBTREE_CONTROL   "/cgroup/test2/cgroup.subtree_control"
+#define TEST_2_MEM_MIN                  "/cgroup/test2/memory.min"
 #define ROOT_CGROUP_PROCS               "/cgroup/cgroup.procs"
+
+#define TEST_TMP_CGROUP_SUBTREE_CONTROL   "/cgroup/testtmp/cgroup.subtree_control"
+#define TEST_TMP_MEM_MIN                  "/cgroup/testtmp/memory.min"
 
 #define TEMP_FILE "temp_file"
 
