9abd436a12121c29c5af40c4fe21dd8b033a9f34
Baruch-Fridman <bruchfridman@gmail.com>
2022-01-24 12:20:05 -0800

    cgroup memory controller - memory.min
    Add support for memory.min
    When declaring memory.min updates a counter of saved pages that can only be assigned to this group.
    If there are not enough free pages to save the statement you will fail.
    If you try to allocate memory, such as creating a process, but there are no unsaved pages [the number of free pages equals the number of saved pages] the request will be rejected.
    When allocating memory to a group for which pages have been saved, the number of saved pages will decrease.
    
    Change-Id: I13b1b5c417c97a89067fb647608569694f5269e2

--- vm.c
+++ vm.c
@@ -6,6 +6,7 @@
 #include "mmu.h"
 #include "proc.h"
 #include "elf.h"
+#include "cgroup.h"
 
 extern char data[];  // defined by kernel.ld
 pde_t *kpgdir;  // for use in scheduler()
@@ -219,33 +220,41 @@ loaduvm(pde_t *pgdir, char *addr, struct inode *ip, uint offset, uint sz)
 // Allocate page tables and physical memory to grow process from oldsz to
 // newsz, which need not be page aligned.  Returns new size or 0 on error.
 int
-allocuvm(pde_t *pgdir, uint oldsz, uint newsz)
+allocuvm(pde_t* pgdir, uint oldsz, uint newsz, struct cgroup* cgroup)
 {
-  char *mem;
-  uint a;
+    char* mem;
+    uint a;
+    int set_cnt = 0;
 
-  if(newsz >= KERNBASE)
-    return 0;
-  if(newsz < oldsz)
-    return oldsz;
+    if (newsz >= KERNBASE)
+        return 0;
+    if (newsz < oldsz)
+        return oldsz;
 
-  a = PGROUNDUP(oldsz);
-  for(; a < newsz; a += PGSIZE){
-    mem = kalloc();
-    if(mem == 0){
-      cprintf("allocuvm out of memory\n");
-      deallocuvm(pgdir, newsz, oldsz);
-      return 0;
+    a = PGROUNDUP(oldsz);
+    for (; a < newsz; a += PGSIZE) {
+
+        if (dec_protect_mem(cgroup) == 0) {
+            set_cnt++;
+        }
+
+        mem = kalloc();
+        if (mem == 0) {
+            cprintf("allocuvm out of memory\n");
+            deallocuvm(pgdir, newsz, oldsz);
+            inc_protect_mem(cgroup, set_cnt);
+            return 0;
+        }
+        memset(mem, 0, PGSIZE);
+        if (mappages(pgdir, (char*)a, PGSIZE, V2P(mem), PTE_W | PTE_U) < 0) {
+            cprintf("allocuvm out of memory (2)\n");
+            deallocuvm(pgdir, newsz, oldsz);
+            kfree(mem);
+            inc_protect_mem(cgroup, set_cnt);
+            return 0;
+        }
     }
-    memset(mem, 0, PGSIZE);
-    if(mappages(pgdir, (char*)a, PGSIZE, V2P(mem), PTE_W|PTE_U) < 0){
-      cprintf("allocuvm out of memory (2)\n");
-      deallocuvm(pgdir, newsz, oldsz);
-      kfree(mem);
-      return 0;
-    }
-  }
-  return newsz;
+    return newsz;
 }
 
 // Deallocate user pages to bring the process size from oldsz to
@@ -276,6 +285,24 @@ deallocuvm(pde_t *pgdir, uint oldsz, uint newsz)
     }
   }
   return newsz;
+}
+
+int
+dec_protect_mem(struct cgroup* cgroup)
+{
+
+    if (cgroup != cgroup_root() && cgroup->mem_controller_enabled && cgroup->protected_mem > 0) {
+        decrese_protect_counter(1);
+        cgroup->protected_mem -= 1;
+        return 0;
+    }
+
+    return 1;
+}
+
+void inc_protect_mem(struct cgroup* cgroup, int n) {
+    increse_protect_counter(n);
+    cgroup->protected_mem += n;
 }
 
 // Free a page table and all the physical memory pages
@@ -389,4 +416,3 @@ copyout(pde_t *pgdir, uint va, void *p, uint len)
 // Blank page.
 //PAGEBREAK!
 // Blank page.
-
