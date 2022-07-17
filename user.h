f004789d10070253559399d331e6b8880595b4b6
Yakov Mor Atlas <yakovmora1@gmail.com>
2022-07-17 21:00:10 +0300

    Add kernel's total memory to memory controller stat file
    Add kernels' total memory to memory.stat file which is one of the memory
    contoller's core file. This is according to linux cgroup2 specification.
    
    Change-Id: I48dd6e0f5d809443856230faee3948b082d6c089

--- user.h
+++ user.h
@@ -47,10 +47,11 @@ int unshare(int);
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
-void *memmove(void*, const void*, int);
+void* memmove(void*, const void*, int);
 char* strchr(const char*, char c);
 int strcmp(const char*, const char*);
 int strncmp(const char*, const char*, int);
+char* strstr(char * src, char * needle);
 int printf(int, const char*, ...);
 char* gets(char*, int max);
 uint strlen(const char*);
