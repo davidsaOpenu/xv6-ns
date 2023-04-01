208bf0e09bc0e4161fee18439907f1b173881975
David Sariel <davidsa@openu.ac.il>
2023-04-01 09:07:02 -0400

    cgroup module - add return status enum
    Refactor cgroup module to return enum instead of integer values.
    The name of the enum is result_t.
    Change relevant functions to use the enum.
    
    Change-Id: Id515e48312bda2b7ba9505ee3a214890eda57cab

--- defs.h
+++ defs.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_GUARD_DEFS_H
+#define HEADER_GUARD_DEFS_H
+
 struct buf;
 struct context;
 struct file;
@@ -268,3 +271,19 @@ int             intlen(int n);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
+
+
+/** Return codes:
+ * - RESULT_FAILURE upon failure (error).
+ * - RESULT_SUCCESS upon general success - no errors.
+ * - RESULT_SUCCESS_OPERATION upon successful (effective) operation.
+*/
+typedef enum result_e
+{
+	RESULT_FAILURE = -1,
+	RESULT_SUCCESS,
+	RESULT_SUCCESS_OPERATION
+} result_t;
+
+
+#endif
