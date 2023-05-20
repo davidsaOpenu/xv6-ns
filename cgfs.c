f8a1073b29705988150bec84aeaa2a1c1a54140d
Yonatan Roth <yonatanroth1@gmail.com>
2023-05-20 11:25:23 -0400

    [cgroup fs] - disabled cpu controller inconsistent behavior
        Consider cpu controller state when running cat or ls commands on cpu.max.
        Make the commands fail when controller is disabled.
        Achieve consistent behavior with running ls command on parent folder.
    
    Trello-card: https://trello.com/c/nypxADrf
    Change-Id: I31e5363bf03af1c8f5a632bd06082402732abf6c

--- cgfs.c
+++ cgfs.c
@@ -254,6 +254,16 @@ static cgroup_file_name_t get_file_name_constant(char * filename)
     return -1;
 }
 
+int is_cg_file_statable(struct file * f)
+{
+	if (get_file_name_constant(f->cgfilename) == CPU_MAX &&
+			!f->cgp->cpu_controller_enabled) {
+		return 0;
+	}
+
+	return 1;
+}
+
 /**
  * This function opens a cgroup dir
  */
@@ -855,7 +865,10 @@ static int unsafe_cg_read_file(struct file * f, char * addr, int n)
             break;
 
         case CPU_MAX:
-            r = read_file_cpu_max(f, addr, n);
+            if(f->cgp->cpu_controller_enabled) {
+                r = read_file_cpu_max(f, addr, n);
+            }
+
             break;
         
         case PID_MAX:
@@ -1434,6 +1447,10 @@ int unsafe_cg_stat(struct file * f, struct stat * st)
         st->size = MAX_CGROUP_FILE_NAME_LENGTH *
                    (7 + cgorup_num_of_immidiate_children(f->cgp));
     } else {
+        if(!is_cg_file_statable(f)) {
+            return -1;
+        }
+
         st->type = T_CGFILE;
         st->size = cg_file_size(f);
     }
